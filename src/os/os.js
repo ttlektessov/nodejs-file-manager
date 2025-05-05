979e0b01c26994e2a51cb419960b04f6c000d4ff
Temirlan Tlektessov <ttlektessov@gmail.com>
2025-05-06 02:42:51 +0500

    feat: os cpus complete

--- src/os/os.js
+++ src/os/os.js
@@ -5,6 +5,16 @@ export const handleOSCommand = (flag) => {
     case "--EOL":
       console.log(JSON.stringify(os.EOL));
       break;
+    case "--cpus":
+      const cpus = os.cpus();
+      console.log(`Total CPUs: ${cpus.length}`);
+      cpus.forEach((cpu, index) => {
+        const { model, speed } = cpu;
+        console.log(
+          `CPU ${index + 1}: ${model}, ${(speed / 1000).toFixed(2)} GHz`
+        );
+      });
+      break;
 
     default:
       console.log("Invalid input");
