1066402cfe5368b3416284cfb734810d3e03a076
Temirlan Tlektessov <ttlektessov@gmail.com>
2025-05-05 17:27:11 +0500

    feat: index.js console management

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import readline from "readline";
 
 const args = process.argv.slice(2);
 const username = process.env.npm_config_username || "unknown";
+let currDir = homedir();
 
 console.log(`Welcome to the File Manager, ${username}!`);
-
-let currDir = homedir();
 console.log(`You are currently in ${currDir}`);
+
+const printDir = () => {
+  console.log(`You are currently in ${currDir}`);
+};
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -15,7 +18,23 @@ const rl = readline.createInterface({
   prompt: "> ",
 });
 
-rl.on("SIGINT", () => {
+rl.on("line", async (line) => {
+  const command = line.trim();
+
+  if (command === ".exit") {
+    rl.close();
+    return;
+  }
+  console.log("Invalid input.");
+  printDir();
+  rl.prompt();
+});
+
+rl.on("close", () => {
   console.log(`\nThank you for using File Manager, ${username}, goodbye!`);
   process.exit(0);
+});
+
+rl.on("SIGINT", () => {
+  rl.close();
 });
